7796439dda07a1626aebcfa4f5f20b3f1bbbce1a
nikolliervin <AG@DESKTOP-T1PBK0V>
2023-11-15 23:03:55 +0100

    Review cors policy

--- ocr/main.py
+++ ocr/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
 from application import route
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [  
+    "chrome-extension://kcmfgkjmdhbhlmfkkebomchhnlpedknk",  
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(route.router)
