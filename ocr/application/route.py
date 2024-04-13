9f1cb1dfea22997628ccf393510c0232347d8156
nikolliervin <AG@DESKTOP-T1PBK0V>
2024-04-13 13:21:08 +0200

    Added get method

--- ocr/application/route.py
+++ ocr/application/route.py
@@ -4,6 +4,11 @@ from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
+@router.get('/')
+async def root():
+    return {'message':'Use /recognize'}
+
+
 @router.post("/recognize")
 async def recognize_text(image: UploadFile):
     return await recognize_controller.recognize_text(image)
